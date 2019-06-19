4e141da8601fcff05bbe193a3fa47f90fd80bcca
Dariia Zhyr <darzhyr@gmail.com>
2019-06-19 23:51:22 +0300

    fixed

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 'use strict';
 
 $(document).ready(function() {
-    $(".to_top").click(function () {
-    let elementClick = $((this).getAttribute("href"));
-    let destination = $(elementClick).offset().top;
+  $('.to_top').click(function() {
+    const elementClick = $((this).getAttribute('href'));
+    const destination = $(elementClick).offset().top;
     event.preventDefault();
-    $("body,html").animate({scrollTop: destination }, 600);
-    });
+    $('body,html').animate({ scrollTop: destination }, 600);
   });
+});
