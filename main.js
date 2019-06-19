31d1b9294f64a7a9d32149403e25ca97e1d28021
Dariia Zhyr <darzhyr@gmail.com>
2019-06-19 17:36:06 +0300

    improved appearance of page

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+$(document).ready(function() {
+    $(".to_top").click(function () {
+    let elementClick = $((this).getAttribute("href"));
+    let destination = $(elementClick).offset().top;
+    event.preventDefault();
+    $("body,html").animate({scrollTop: destination }, 600);
+    });
+  });
